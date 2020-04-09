1f511166f84d0d887964eee143806a451497c470
Marc Guerrini <maguerrini@airfrance.fr>
2020-04-09 21:37:08 +0200

    article2

---
+++ secured-server/src/main/java/com/mgu/spring/security/config/Oauth2SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.mgu.spring.security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class Oauth2SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // @formatter:off
+        http.authorizeRequests(a -> a.anyRequest().authenticated())
+            .oauth2Login();
+        // @formatter:on
+    }
+}
